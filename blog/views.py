d1b439c21c3e57604c0b6db9e53177f10e4d25d8
decant09 <>
2023-10-12 13:51:45 +0000

    Update context_object_name to posts

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,6 @@ from .models import Post
 class PostListView(generic.ListView):
     model = Post
     ordering = ['-created_on']
+    context_object_name = 'posts'
     template_name = "index.html"
     paginate_by = 6
