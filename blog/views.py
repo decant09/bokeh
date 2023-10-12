48852e55a39a2284ed1d0030b8948160a73838bc
decant09 <>
2023-10-12 11:55:21 +0000

    Add view code, template & connect urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+    ordering = ['-created_on']
+    template_name = "index.html"
+    paginate_by = 6
