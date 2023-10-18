6389990ceaadc7928ec30630298c58fa1780d730
decant09 <>
2023-10-18 12:50:15 +0000

    Add update profile functionality

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,23 @@
-from .models import Comment
 from django import forms
+from django.contrib.auth.models import User
+from .models import Comment, Profile
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class EditUserForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['about_user', 'image']
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.views import generic, View
 from django.contrib.auth.decorators import login_required
 from .models import Post, Profile
-from .forms import CommentForm
+from .forms import CommentForm, EditUserForm, EditProfileForm
 
 
 def index(request):
@@ -64,5 +64,25 @@ class PostDetailView(generic.DetailView):
 
 @login_required
 def profile(request):
-    profile = Profile.objects.get(user=request.user)
-    return render(request, 'profile.html', {'profile': profile})
+    if request.method == 'POST':
+        profile = Profile.objects.get(user=request.user)
+        edit_user = EditUserForm(request.POST, instance=request.user)
+        edit_profile = EditProfileForm(request.POST,
+                                       request.FILES,
+                                       instance=request.user.profile)
+        if edit_user.is_valid() and edit_profile.is_valid():
+            edit_user.save()
+            edit_profile.save()
+            messages.success(request, f'Profile updated successfully')
+            return redirect('profile')
+    else:
+        profile = Profile.objects.get(user=request.user)
+        edit_user = EditUserForm(instance=request.user)
+        edit_profile = EditProfileForm(instance=request.user.profile)
+
+    context = {
+        'profile': profile,
+        'edit_user': edit_user,
+        'edit_profile': edit_profile
+    }
+    return render(request, 'profile.html', context)
